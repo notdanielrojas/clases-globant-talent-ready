f57f45973881640d641f07cabfd6a976e43a774a
notdanielrojas <notdanielrojas@gmail.com>
2026-08-19 22:56:34 -0400

    Prueba de cucumber

--- tests/features/support/hooks.js
+++ tests/features/support/hooks.js
@@ -6,72 +6,9 @@ import {
     setDefaultTimeout
 } from '@cucumber/cucumber';
 
-import { chromium } from 'playwright';
+import { chromium } from '@playwright/test';
 import { existsSync, mkdirSync } from 'fs';
-import { LoginPage } from '../../pages/LoginPage';
-import { Given, When, Then } from '@cucumber/cucumber';
-import { expect } from '@playwright/test';
-
-
-Given('the user is on the Saucedemo login page', async function () {
-  await this.loginPage.navigate();
-});
-
-When(
-  'the user enters username {string} and password {string}',
-  async function (username, password) {
-    await this.loginPage.fillUsername(username);
-    await this.loginPage.fillPassword(password);
-  }
-);
-
-When('the user leaves username and password fields empty', async function () {
-  await this.loginPage.fillUsername('');
-  await this.loginPage.fillPassword('');
-});
-
-When(
-  'the user leaves username field empty and enters password {string}',
-  async function (password) {
-    await this.loginPage.fillUsername('');
-    await this.loginPage.fillPassword(password);
-  }
-);
-
-When(
-  'the user enters username {string} and leaves password field empty',
-  async function (username) {
-    await this.loginPage.fillUsername(username);
-    await this.loginPage.fillPassword('');
-  }
-);
-
-When('clicks the login button', async function () {
-  await this.loginPage.clickLogin();
-});
-
-Then('the user is redirected to the products page', async function () {
-  await expect(this.page).toHaveURL(/inventory\.html/);
-});
-
-Then(
-  'the user should see the error message {string}',
-  async function (message) {
-    await expect(this.loginPage.errorMessage).toHaveText(message);
-  }
-);
-
-Then(
-  'the user should see the expected outcome {string} with the message {string}',
-  async function (outcome, message) {
-    if (outcome === 'success') {
-      await expect(this.page).toHaveURL(/inventory\.html/);
-      return;
-    }
-
-    await expect(this.loginPage.errorMessage).toHaveText(message);
-  }
-);
+import { LoginPage } from '../../pages/loginPage.js';
 
 setDefaultTimeout(60 * 1000);
 
@@ -82,15 +19,12 @@ if (!existsSync(screenshotDir)) {
 }
 
 Before(async function () {
-  
     this.browser = await chromium.launch({
-        headless: false
+        headless: process.env.CI ? true : false
     });
 
     this.context = await this.browser.newContext();
-
     this.page = await this.context.newPage();
-
     this.loginPage = new LoginPage(this.page);
 });
 
@@ -114,7 +48,7 @@ AfterStep(async function ({ pickle, result }) {
             fullPage: true
         });
 
-        await this.attach(screenshot, 'image/png');
+        this.attach(screenshot, 'image/png');
     }
 });
 
